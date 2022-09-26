52a8b2360e832e20d09a1784d7a9bc81bfb054c4
root <root@DESKTOP-TPQ0LCA.localdomain>
2022-09-27 00:09:45 +0300

    advanced

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' module: 101-add_attribute.py
+'''
+
+
+def add_attribute(*args):
+    '''raises a TypeError with the message
+       returns None
+    '''
+    if "main" in str(type(args[0])):
+        setattr(args[0], args[1], args[2])
+    else:
+        raise TypeError("can't add new attribute")
+
