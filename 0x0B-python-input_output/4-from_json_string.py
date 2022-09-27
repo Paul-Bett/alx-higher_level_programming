1d7b315e17f9197d9ef8de61befdfc517a6543d7
root <root@DESKTOP-TPQ0LCA.localdomain>
2022-09-27 22:09:35 +0300

    update

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -6,8 +6,7 @@ import json
 
 
 def from_json_string(my_str):
-    '''Function: Returns an object (Python data structure) represented by a JSON string  module from_json_string
+    '''Function: Returns an object represented by a JSON string
        Returns: Python objects
     '''
     return json.loads(my_str)
-
