e4b28fbc3cf4195160e0f800f94e990e9e44dfa4
Paul-Bett <bettpaul6@gmail.com>
2023-09-04 20:08:42 +0300

    substandardupdate

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+// script that prints the addition of 2 integers
 const args = process.argv;
-let A = parseInt(args[2]);
-let B = parseInt(args[3]);
+const A = parseInt(args[2]);
+const B = parseInt(args[3]);
 if (typeof A === 'number' && typeof B === 'number') {
   console.log(add(A, B));
 } else {
